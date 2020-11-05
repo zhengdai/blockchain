335708d5d089d1e2c2d43fa10678d5473e1b73b5
zhengdai <798484315@qq.com>
2020-11-05 14:23:12 +0800

    修复bug

--- chain/utils.go
+++ chain/utils.go
@@ -17,6 +17,6 @@ func IntToHex(num int64) []byte {
 
 func ReverseBytes(data []byte) {
 	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
-		data[i], data[j] = data[j], data[j]
+		data[i], data[j] = data[j], data[i]
 	}
 }
